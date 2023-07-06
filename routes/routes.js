64597d5ba272b0efac43823a3679e77dd0236e19
RackelFonseca <67659007+RackelFonseca@users.noreply.github.com>
2023-07-06 20:49:29 +0000

    Initial commit

---
+++ routes/routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const Services = require("../services/services");
+const router = express.Router();
+/*
+router.get("/", (req, res) => {
+  res.send("Seja bem Vindo ao nosso sistema de Varejo Virtual.");
+});
+*/
+
+router.get("/", Services.LivroListar);
+
+router.post("/login", Services.FuncionarioLogin);
+router.get("/login", (req, res) => {
+  res.render("funcionarios/login");
+})
+router.post("/login", Services.FuncionarioLogin)
+
+
+router.get("/funcionarios/Cadastrar", (req, res) => {
+  res.render("funcionarios/Cadastrar");
+})
+router.post("/funcionarios/Cadastrar", Services.FuncionarioCreate);
+
+router.get("/livros/Cadastrar", (req, res) => {
+  res.render("livros/Cadastrar");
+})
+router.post("/livros/Cadastrar", Services.LivroCreate);
+
+router.get("/livros/listar", Services.LivroListar);
+
+module.exports = router; 
---
+++ services/services.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+module.exports = class Services {
+  //VERIFICAR Funcionario
+  static async FuncionarioLogin(req, res) {
+    let valores = req.body;
+    const options = {
+      url: 'https://apilivraria.rackelcoelho.repl.co/login',
+      method: 'POST',
+      data: valores
+    };
+    axios(options).then((funcionario) => {
+      if (funcionario != undefined) {
+        return res.render('logado');
+      }
+    })
+  }
+  //Create Funcionario 
+  static async FuncionarioCreate(req, res) {
+    let valores = req.body;
+    const options = {
+      url: 'https://apilivraria.rackelcoelho.repl.co/add_funcionario',
+      method: 'POST',
+      data: valores
+    };
+    axios(options);
+    const mensagem = "Cadastro realizado com sucesso!";
+    res.render("mensagem", { mensagem });
+  }
+  //Create livro
+  static async LivroCreate(req, res) {
+    let valores = req.body;
+    const options = {
+      url: 'https://apilivraria.rackelcoelho.repl.co/add_livros',
+      method: 'POST',
+      data: valores
+    };
+    axios(options);
+    const mensagem = "Cadastro realizado com sucesso!";
+    res.render("mensagem", { mensagem });
+  }
+  //LISTAR 
+  static async LivroListar(req, res) {
+    const options = {
+      url: 'https://apilivraria.rackelcoelho.repl.co/livros',
+      method: 'GET',
+      data: {}
+    };
+    axios(options).then(response => {
+      console.log(response.data);
+      const livro = response.data
+      res.render("livros/listar", { livro });
+    });
+  }
+} 
